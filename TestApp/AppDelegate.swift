8d24501d9f99c65ae1d94dd81ea33f8700ed289d
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-08-27 01:08:21 +0200

    Delete storyboard, synx project, add main class code, edit appDelegate

--- TestApp/AppDelegate.swift
+++ TestApp/AppDelegate.swift
@@ -1,9 +1,6 @@
 import Cocoa
 
-@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -12,7 +9,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
--- TestApp/main.swift
+++ TestApp/main.swift
@@ -1,1 +1,5 @@
-import Foundation
+import Cocoa
+
+let appDelegate = AppDelegate()
+NSApplication.shared().delegate = appDelegate
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
