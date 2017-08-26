aca58812bddce01687b612ffb0bd583838b955ca
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-08-27 01:00:25 +0200

    Initial commit

---
+++ TestApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by Michał Czerniakowski on 27.08.2017.
+//  Copyright © 2017 m-code. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Michał Czerniakowski on 27.08.2017.
+//  Copyright © 2017 m-code. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
